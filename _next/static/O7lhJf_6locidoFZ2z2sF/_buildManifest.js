3c2d80e1fd6b71cc34f66651b4509bd95302f06b
dmtahanigit <dmtahanigit@users.noreply.github.com>
2025-03-10 20:38:00 -0400

    Updates

--- _next/static/O7lhJf_6locidoFZ2z2sF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/335-4ed2634b910abe6f.js","static/chunks/pages/index-5fba2fe390b1a625.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
