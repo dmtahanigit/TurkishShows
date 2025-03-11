cfc551fee203fb03e633db3fefc8df93406d36aa
dmtahanigit <dmtahanigit@users.noreply.github.com>
2025-03-10 20:28:38 -0400

    Updates

--- _next/static/85aF6DakNnxOApiLt97SM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/908-179ddc7b7f89cc1d.js","static/chunks/pages/index-5b8ad118a1782e71.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/cw3ROK1NlwZGKcoB-B0hO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/908-179ddc7b7f89cc1d.js","static/chunks/pages/index-ce03aeec27b9fdc9.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
